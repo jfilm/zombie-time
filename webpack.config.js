1f78debd64f6fb613a649c5c7d8d8f2c6468b46d
Bohdan <bohdan.kornatskyi@gmail.com>
2021-08-24 21:37:37 -0700

    made changes to the webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,56 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin'); //installed via npm
 
 module.exports = {
-  mode: "development",
-  entry: './src/index.js',
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader']
-      }
-    ]
-  },
-  devServer: {
-    static: {
-      directory: path.join(__dirname, "dist"),
-    }
-  }
+    entry:  './src/index.js',
+    devtool: 'inline-source-map',
+    devServer: {
+        contentBase: path.join(__dirname, 'dist'),
+        hot: true,
+        port: 8080,
+        host:"127.0.0.1"
+    },
+
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'main.js',
+    },
+    mode: 'development',
+    module: {
+        rules: [
+            {
+                test: /\.txt$/,
+                use: 'raw-loader'
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    // Creates `style` nodes from JS strings
+                    "style-loader",
+                    // Translates CSS into CommonJS
+                    "css-loader",
+                    // Compiles Sass to CSS
+                    "sass-loader",
+
+                ],
+            },
+            {
+                test: /\.css$/i,
+                use: [
+                    // Creates `style` nodes from JS strings
+                    "style-loader",
+                    // Translates CSS into CommonJS
+                    "css-loader",
+
+                ],
+            },
+            { test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "file-loader" },
+            //removed svg from regex , for some reason url-loader brokes svg import
+            { test: /\.(png|jpg|jpeg|gif)$/, loader: "url-loader" },
+            { test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "url-loader" },
+
+        ],
+    },
+    plugins: [new HtmlWebpackPlugin({ template: './src/index.html', minify: false })],
+
 };
