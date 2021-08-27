c02d9bc844e44f6fd459e1b7218b23cb7b694346
Hunter Marshall <beidah@gmail.com>
2021-08-26 19:27:51 -0500

    Back to circle for enemies and player

--- src/Entities/Enemy.js
+++ src/Entities/Enemy.js
@@ -14,20 +14,18 @@ export class Enemy extends Entity {
             x: Math.cos(angle),
             y: Math.sin(angle)
         }
-
-
     }
 
 
-    draw(ctx) {
-        //This code helps to see the border of an entity
-        // ctx.beginPath();
-        // ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
-        // ctx.fillStyle = this.color;
-        // ctx.fill();
+    // draw(ctx) {
+    //     //This code helps to see the border of an entity
+    //     // ctx.beginPath();
+    //     // ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+    //     // ctx.fillStyle = this.color;
+    //     // ctx.fill();
 
-        //Draw and center the image
-        ctx.drawImage(zombieImg, this.x - this.radius, this.y - this.radius, this.radius * 2, this.radius * 2);
-    }
+    //     //Draw and center the image
+    //     ctx.drawImage(zombieImg, this.x - this.radius, this.y - this.radius, this.radius * 2, this.radius * 2);
+    // }
 
 }
--- src/Entities/Player.js
+++ src/Entities/Player.js
@@ -12,18 +12,6 @@ export class Player extends Entity {
         this.weapon.playerX = this.x;
         this.weapon.playerY = this.y
     }
-
-    draw(ctx) {
-
-        //This code helps to see the border of an entity
-        // ctx.beginPath();
-        // ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
-        // ctx.fillStyle = this.color;
-        // ctx.fill();
-
-        //Draw and center the image
-        ctx.drawImage(playerImg, this.x - this.radius, this.y - this.radius, this.radius * 2, this.radius * 2);
-    }
 }
 
 
