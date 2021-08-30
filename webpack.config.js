6f44f8b8e73918dd4505dab5694f453f2a59e8a6
John Asher <6667623+jfilm@users.noreply.github.com>
2021-08-30 15:44:52 -0500

    Refactored webpage to add landing page

--- webpack.config.js
+++ webpack.config.js
@@ -1,64 +1,69 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin'); //installed via npm
-const CopyWebpackPlugin = require('copy-webpack-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
-    entry: './src/index.js',
-    devtool: 'inline-source-map',
-    devServer: {
-        contentBase: path.join(__dirname, 'dist'),
-        hot: true,
-        port: 8080,
-        host: "127.0.0.1"
-    },
+  entry: {
+    index: './src/index.js',
+    game: './src/game.js',
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    hot: true,
+    port: 8080,
+    host: '127.0.0.1',
+  },
 
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'main.js',
-    },
-    mode: 'development',
-    module: {
-        rules: [
-            {
-                test: /\.txt$/,
-                use: 'raw-loader'
-            },
-            {
-                test: /\.s[ac]ss$/i,
-                use: [
-                    // Creates `style` nodes from JS strings
-                    "style-loader",
-                    // Translates CSS into CommonJS
-                    "css-loader",
-                    // Compiles Sass to CSS
-                    "sass-loader",
-
-                ],
-            },
-            {
-                test: /\.css$/i,
-                use: [
-                    // Creates `style` nodes from JS strings
-                    "style-loader",
-                    // Translates CSS into CommonJS
-                    "css-loader",
-
-                ],
-            },
-            { test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "file-loader" },
-            //removed svg from regex , for some reason url-loader brokes svg import
-            { test: /\.(png|jpg|jpeg|gif)$/, loader: "url-loader" },
-            { test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "url-loader" },
-
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].js',
+  },
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.txt$/,
+        use: 'raw-loader',
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          'style-loader',
+          // Translates CSS into CommonJS
+          'css-loader',
+          // Compiles Sass to CSS
+          'sass-loader',
         ],
-    },
-    plugins: [
-        new HtmlWebpackPlugin({ template: './src/index.html', minify: false }),
-        new CopyWebpackPlugin({
-            patterns: [
-                { from: 'src/assets', to: 'assets' }
-            ]
-        })],
-
-
+      },
+      {
+        test: /\.css$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          'style-loader',
+          // Translates CSS into CommonJS
+          'css-loader',
+        ],
+      },
+      {
+        test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: 'file-loader',
+      },
+      //removed svg from regex , for some reason url-loader brokes svg import
+      { test: /\.(png|jpg|jpeg|gif)$/, loader: 'url-loader' },
+      { test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: 'url-loader' },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({ template: './src/index.html', minify: false }),
+    new HtmlWebpackPlugin({
+      template: './src/game.html',
+      minify: false,
+      filename: 'game.html',
+    }),
+    new CopyWebpackPlugin({
+      patterns: [{ from: 'src/assets', to: 'assets' }],
+    }),
+  ],
 };
